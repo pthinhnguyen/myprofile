67fda3347ac0c21927ff81451905fcf9e3217a01
Ted Thinh Nguyen <tednguyen@Teds-MacBook-Pro.local>
2023-05-03 20:49:50 -0500

    initialized my page layout

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+/**
+ * Screen Size
+ */
+function isMobileScreen() {
+    let screenHeight = window.innerHeight;
+    let screenWidth = window.innerWidth;
+
+    if (screenWidth <= 1000 || screenWidth / screenHeight <= 9/24) return true;
+    return false;
+}
+
+/**
+ * Navigation
+ */
+
 $(function(){
   // change active
   $(document).on("scroll", onScroll);
@@ -53,24 +68,86 @@ function toggleNavbar() {
     header.classList.toggle("active");
 }
 
+/**
+ * Project Slideshow
+ */
 
-// const hamburger = document.querySelector(".hamburger");
-// const verticalNavbar = document.getElementById("vertical-navbar");
+let currentSlideOrder = 1;
+let projectSlideShowItemId = "project-slideshow-item";
+let projectCardId = "project-card";
+let projectCardLeftPaneId = "project-card-left-pane";
+let projectCardRightPaneId = "project-card-right-pane";
 
-// // hamburger.addEventListener("click", mobileMenu);
+let projectSlidesShowItems = document.getElementsByClassName(projectSlideShowItemId);
+let projectCardItems = document.getElementsByClassName(projectCardId);
+let projectCardLeftPaneItems = document.getElementsByClassName(projectCardLeftPaneId);
+let projectCardRightPaneItems = document.getElementsByClassName(projectCardRightPaneId);
 
-// // function mobileMenu() {
-// //     hamburger.classList.toggle("active");
-// //     navMenu.classList.toggle("active");
-// // }
+showSlides(currentSlideOrder);
+
+// avoid multiple click
+let projectTimeTransition = 500
+let isProjectNavigationButtonClicked = false;
+
+function plusSlides(numberOfSlides) {
+    if (!isProjectNavigationButtonClicked) {
+        // clicked
+        isProjectNavigationButtonClicked = true;
+
+        // set the current slide to back
+        projectSlidesShowItems[currentSlideOrder - 1].style.zIndex = '0';
+
+        // show the next slide
+        let newSlideOrder = showSlides(currentSlideOrder + numberOfSlides);
+        projectSlidesShowItems[currentSlideOrder - 1].style.zIndex = '0';
+
+        projectCardRightPaneItems[currentSlideOrder - 1].classList.toggle("active");
+
+        if (isMobileScreen()) {
+            projectCardRightPaneItems[currentSlideOrder - 1].style.left = '-100%';
+        } else {
+            projectCardRightPaneItems[currentSlideOrder - 1].style.top = '-100%';
+        }
+
+        // close the current slide
+        setTimeout(function () {
+            projectSlidesShowItems[currentSlideOrder - 1].style.visibility = 'hidden';
+
+            if (isMobileScreen()) {
+                projectCardLeftPaneItems[currentSlideOrder - 1].style.left = '-100%';
+                projectCardRightPaneItems[currentSlideOrder - 1].style.left = '100%';
+            } else {
+                projectCardLeftPaneItems[currentSlideOrder - 1].style.top = '-100%';
+                projectCardRightPaneItems[currentSlideOrder - 1].style.top = '100%';
+            }
+
+            currentSlideOrder = newSlideOrder;
+        }, projectTimeTransition)
 
 
-// function toggleNavbar() {
-//   hamburger.classList.toggle("active");
-//   var displaySetting = window.getComputedStyle(verticalNavbar).display;
-//   if (displaySetting == "grid") {
-//     verticalNavbar.style.display = "none";
-//   } else {
-//     verticalNavbar.style.display = "grid";
-//   }
-// }
+        setTimeout(function() {
+            isProjectNavigationButtonClicked = false;
+        }, projectTimeTransition * 2)
+    }
+}
+
+function showSlides(slideOrder) {
+
+    if (slideOrder > projectSlidesShowItems.length) slideOrder = 1; // circling to the first slides
+    if (slideOrder < 1) slideOrder = projectSlidesShowItems.length; // circling to the last slide
+
+    // activate the current slide
+    projectSlidesShowItems[slideOrder - 1].style.zIndex = '1';
+    projectSlidesShowItems[slideOrder - 1].style.visibility = 'visible';
+
+    projectCardRightPaneItems[slideOrder - 1].classList.toggle("active");
+    if (isMobileScreen()) {
+        projectCardLeftPaneItems[slideOrder - 1].style.left = '0';
+        projectCardRightPaneItems[slideOrder - 1].style.left = '0';
+    } else {
+        projectCardLeftPaneItems[slideOrder - 1].style.top = '0';
+        projectCardRightPaneItems[slideOrder - 1].style.top = '0';
+    }
+
+    return slideOrder;
+}
