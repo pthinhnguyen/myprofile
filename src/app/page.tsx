00676c1b2e8e54c50b5543ed280c33998d4ac952
Ted Nguyen <ngpthinh11@gmail.com>
2026-03-16 12:25:28 -0500

    feat: scaffold initial portfolio sections and update project dependencies.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import AboutSection from "./about/page";
-import ProjectsSection from "./projects/page";
-import ContactSection from "./contact/page";
-import HomeSection from "./home/page";
+import AboutSection from "@/components/sections/About";
+import ProjectsSection from "@/components/sections/Projects";
+import ContactSection from "@/components/sections/Contact";
+import HomeSection from "@/components/sections/Home";
 import styles from "./page.module.css";
 
 export default function LandingPage() {
